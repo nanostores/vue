a98dbee076561a28b37f6b11372e728281855229
Eduard Aksamitov <e@edaks.co>
2021-10-29 16:36:31 +0300

    feat: use new color

--- devtools/index.js
+++ devtools/index.js
@@ -222,7 +222,7 @@ function createTemplateLogger(app, api, template, templateName, nameGetter) {
       {
         label: 'Template',
         textColor: 0xffffff,
-        backgroundColor: 0x0000ff
+        backgroundColor: 0x1f49e0
       }
     ],
     children: []
