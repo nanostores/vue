004a4de19178ef721781b2e04cbca306cee2c646
Eduard Aksamitov <e@edaks.co>
2021-11-02 16:59:13 +0300

    chore(docs): describe api

--- devtools/index.d.ts
+++ devtools/index.d.ts
@@ -1,20 +1,38 @@
 import { AnySyncTemplate, Atom, MapStore } from 'nanostores'
 import { App } from 'vue'
 
-export function devtools(app: App): void
-
 type AnyStore = Atom | MapStore | AnySyncTemplate
 
 export interface StoreNameGetter {
   (store: AnyStore, templateName: string): string
 }
 
+/**
+ * Vue Devtools plugin.
+ *
+ * It detects nanostores in selected component
+ * and add their states to the component inspector.
+ *
+ * @param app Vue app
+ */
+export function devtools(app: App): void
+
+/**
+ * Attaches stores to devtools’ timeline and inspector.
+ *
+ * @param app Vue app
+ * @param stores Object of stores to attach
+ * @param opts Attach options
+ */
 export function attachStores(
   app: App,
   stores: {
     [key: string]: AnyStore
   },
   opts?: {
+    /**
+     * Custom name getter for stores that was built from `MapTemplate`.
+     */
     nameGetter?: StoreNameGetter
   }
 ): void
