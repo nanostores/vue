0e91c79683a49bd0926f67d525d66c151c04e39a
Eduard Aksamitov <e@euaaaio.ru>
2022-07-08 03:29:11 +0300

    chore: cleanup tests

--- use-store/production.test.ts
+++ use-store/production.test.ts
@@ -3,14 +3,14 @@ import { expect, test } from 'vitest'
 import { render } from '@testing-library/vue'
 import { atom } from 'nanostores'
 
-import '../test/set-production.js'
+import './set-production.js'
 import { useStore } from './index.js'
 
-test('does not return readonly state in production mode', () => {
-  let store = atom(() => {})
+test('returns writable state in production', () => {
+  let store = atom()
   render(
     defineComponent(() => {
-      let state = useStore(store, 'ID')
+      let state = useStore(store)
       expect(isReadonly(state)).toBe(false)
       return () => null
     })
