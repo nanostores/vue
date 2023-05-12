826262a9399e897314670047c0a17cbfee2af622
Eduard Aksamitov <e@euaaaio.ru>
2023-05-12 18:27:31 +0300

    fix: broken export for `registerStore`

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-export { useStore } from './use-store/index.js'
+export { useStore, registerStore } from './use-store/index.js'
 export { useVModel } from './use-v-model/index.js'
 export { mapStores } from './map-stores/index.js'
