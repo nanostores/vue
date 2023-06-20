1e28bc4b3293bc94fce8974ce7cf465694a64271
Eduard Aksamitov <e@euaaaio.ru>
2023-06-20 19:10:03 +0300

    refactor(devtools): cleanup and use `@nanostores/logger`

--- devtools/index.js
+++ devtools/index.js
@@ -1,15 +1,10 @@
+import { addLogger } from '@nanostores/logger'
 import { setupDevtoolsPlugin } from '@vue/devtools-api'
-import {
-  lastAction,
-  onSet,
-  onStart,
-  onStop,
-  STORE_UNMOUNT_DELAY
-} from 'nanostores'
+import { onSet } from 'nanostores'
 
 const layerId = 'nanostores'
 const inspectorId = 'nanostores'
-const pluginConfig = {
+export const pluginConfig = {
   componentStateTypes: ['Nanostores'],
   enableEarlyProxy: true,
   homepage: 'https://github.com/nanostores',
@@ -18,14 +13,9 @@ const pluginConfig = {
   logo: 'https://nanostores.github.io/nanostores/logo.svg',
   packageName: '@nanostores/vue',
   settings: {
-    keepUnmounted: {
-      defaultValue: false,
-      label: 'Keep unmounted',
-      type: 'boolean'
-    },
-    realtimeUpdateDetected: {
+    realtime: {
       defaultValue: true,
-      label: 'Real-time update detected',
+      label: 'Real-time update detection',
       type: 'boolean'
     }
   }
@@ -34,12 +24,20 @@ const pluginConfig = {
 let eventGroups = 0
 let inspectorTree = []
 
+function getGroupId() {
+  return (eventGroups += 1)
+}
+
+function pushToInspectorTree(node) {
+  inspectorTree.push(node)
+}
+
 function find(target, text) {
   return target.some(item => item.toLowerCase().includes(text.toLowerCase()))
 }
 
 function isAtom(store) {
-  return typeof store.setKey === 'undefined'
+  return store.setKey === undefined
 }
 
 function notifyComponentUpdate(api, ...args) {
@@ -51,18 +49,150 @@ function notifyComponentUpdate(api, ...args) {
   }
 }
 
-export function devtools(app) {
+function isValidPayload(payload, app, id) {
+  return (
+    payload.app === app &&
+    payload.inspectorId === inspectorId &&
+    payload.nodeId === id
+  )
+}
+
+function createLogger(app, api, store, storeName, groupId, nodeId) {
+  api.sendInspectorTree(inspectorId)
+
+  let unbindLogger = addLogger(store, {
+    set: ({ actionName, changed, newValue, oldValue }) => {
+      api.sendInspectorState(inspectorId)
+      if (api.getSettings().realtime) {
+        notifyComponentUpdate(api)
+      }
+
+      let data = {
+        action: actionName,
+        key: changed,
+        newValue,
+        oldValue
+      }
+      if (data.action === undefined) delete data.action
+      if (data.key === undefined) delete data.key
+
+      api.addTimelineEvent({
+        event: {
+          data,
+          groupId,
+          subtitle: 'was changed',
+          time: Date.now(),
+          title: storeName
+        },
+        layerId
+      })
+    },
+
+    start: () => {
+      api.addTimelineEvent({
+        event: {
+          data: {
+            event: 'mount',
+            store,
+            storeName
+          },
+          groupId,
+          subtitle: 'was mounted',
+          time: Date.now(),
+          title: storeName
+        },
+        layerId
+      })
+    },
+
+    stop: () => {
+      api.addTimelineEvent({
+        event: {
+          data: {
+            event: 'unmount',
+            store,
+            storeName
+          },
+          groupId,
+          subtitle: 'was unmounted',
+          time: Date.now(),
+          title: storeName
+        },
+        layerId
+      })
+    }
+  })
+
+  api.on.getInspectorState(payload => {
+    if (isValidPayload(payload, app, nodeId)) {
+      payload.state = {
+        state: {},
+        store: {
+          active: store.active,
+          listeners: store.lc
+        }
+      }
+      payload.state.state = isAtom(store)
+        ? [
+            {
+              editable: true,
+              key: 'value',
+              value: store.get()
+            }
+          ]
+        : Object.entries(store.get()).map(([key, value]) => ({
+            editable: true,
+            key,
+            value
+          }))
+    }
+  })
+
+  api.on.editInspectorState(payload => {
+    if (isValidPayload(payload, app, nodeId) && store.active !== false) {
+      let { path, state } = payload
+      if (isAtom(store)) {
+        store.set(state.value)
+      } else {
+        store.setKey(path[0], state.value)
+      }
+    }
+  })
+
+  return () => {
+    unbindLogger()
+  }
+}
+
+function createStoreLogger(app, api, store, storeName) {
+  inspectorTree.push({
+    id: storeName,
+    label: storeName
+  })
+  let groupId = getGroupId()
+  createLogger(app, api, store, storeName, groupId, storeName)
+}
+
+export function attachStores(app, stores) {
+  setupDevtoolsPlugin({ ...pluginConfig, app }, api => {
+    for (let [storeName, store] of Object.entries(stores)) {
+      createStoreLogger(app, api, store, storeName)
+    }
+  })
+}
+
+export function devtools(app, stores) {
   setupDevtoolsPlugin({ ...pluginConfig, app }, api => {
     api.addTimelineLayer({
-      color: 0x1f49e0,
+      color: 0x1f_49_e0,
       id: layerId,
-      label: 'Nanostores'
+      label: pluginConfig.label
     })
 
     api.addInspector({
       icon: 'storage',
       id: inspectorId,
-      label: 'Nanostores',
+      label: pluginConfig.label,
       treeFilterPlaceholder: 'Search for stores'
     })
 
@@ -90,14 +220,14 @@ export function devtools(app) {
     let unbindSet = []
     api.on.inspectComponent(payload => {
       if (payload.app === app) {
-        let { realtimeUpdateDetected } = api.getSettings()
+        let { realtime } = api.getSettings()
         if (unbindSet.length > 0) {
           for (let unbind of unbindSet) unbind()
           unbindSet = []
         }
-        let stores = payload.componentInstance.proxy._nanostores || []
-        stores.forEach((store, index) => {
-          if (realtimeUpdateDetected) {
+        let instanceStores = payload.componentInstance.proxy._nanostores || []
+        instanceStores.forEach((store, index) => {
+          if (realtime) {
             unbindSet.push(
               onSet(store, () => {
                 notifyComponentUpdate(api, payload.componentInstance)
@@ -120,10 +250,11 @@ export function devtools(app) {
         payload.type === pluginConfig.componentStateTypes[0]
       ) {
         let {
+          componentInstance,
           path: [index, key],
           state: { newKey, remove, value }
         } = payload
-        let store = payload.componentInstance.proxy._nanostores[index]
+        let store = componentInstance.proxy._nanostores[index]
         if (isAtom(store)) {
           let oldValue = store.get()
           let newValue = value
@@ -138,7 +269,7 @@ export function devtools(app) {
           store.set(newValue)
         } else {
           if (payload.path.length > 2) return
-          if (remove) store.setKey(key, undefined)
+          if (remove) store.setKey(key)
           if (newKey) {
             store.setKey(newKey, value)
           } else {
@@ -148,149 +279,20 @@ export function devtools(app) {
       }
     })
   })
-}
 
-function isValidPayload(payload, app, id) {
-  return (
-    payload.app === app &&
-    payload.inspectorId === inspectorId &&
-    payload.nodeId === id
-  )
-}
-
-function createLogger(app, api, store, storeName, groupId, nodeId) {
-  api.sendInspectorTree(inspectorId)
-
-  let mounted
-  let unbindStart = onStart(store, () => {
-    if (!mounted) {
-      mounted = true
-      api.addTimelineEvent({
-        event: {
-          data: {
-            event: 'mount',
-            store,
-            storeName
-          },
-          groupId,
-          subtitle: 'was mounted',
-          time: Date.now(),
-          title: storeName
-        },
-        layerId
-      })
-    }
-  })
-
-  let unbindStop = onStop(store, () => {
-    setTimeout(() => {
-      if (mounted) {
-        mounted = false
-        api.addTimelineEvent({
-          event: {
-            data: {
-              event: 'unmount',
-              store,
-              storeName
-            },
-            groupId,
-            subtitle: 'was unmounted',
-            time: Date.now(),
-            title: storeName
-          },
-          layerId
-        })
-      }
-    }, STORE_UNMOUNT_DELAY)
-  })
-
-  let unbindSet = onSet(store, ({ changed, newValue }) => {
-    api.sendInspectorState(inspectorId)
-    if (api.getSettings().realtimeUpdateDetected) {
-      notifyComponentUpdate(api)
-    }
-
-    let action = store[lastAction]
-    let data = {
-      action,
-      key: changed,
-      newValue,
-      oldValue: store.get()
-    }
-    if (typeof data.action === 'undefined') delete data.action
-    if (typeof data.key === 'undefined') delete data.key
-    api.addTimelineEvent({
-      event: {
-        data,
-        groupId,
-        subtitle: 'was changed',
-        time: Date.now(),
-        title: storeName
-      },
-      layerId
-    })
-  })
-
-  api.on.getInspectorState(payload => {
-    if (isValidPayload(payload, app, nodeId)) {
-      payload.state = {
-        state: {},
-        store: {
-          active: store.active,
-          listeners: store.lc
-        }
-      }
-      if (isAtom(store)) {
-        payload.state.state = [
-          {
-            editable: true,
-            key: 'value',
-            value: store.get()
-          }
-        ]
-      } else {
-        payload.state.state = Object.entries(store.get()).map(
-          ([key, value]) => ({
-            editable: true,
-            key,
-            value
-          })
-        )
-      }
-    }
-  })
-
-  api.on.editInspectorState(payload => {
-    if (isValidPayload(payload, app, nodeId) && store.active !== false) {
-      let { path, state } = payload
-      if (isAtom(store)) {
-        store.set(state.value)
-      } else {
-        store.setKey(path[0], state.value)
-      }
-    }
-  })
-
-  return () => {
-    unbindStart()
-    unbindStop()
-    unbindSet()
+  if (stores) {
+    attachStores(app, stores)
   }
-}
 
-function createStoreLogger(app, api, store, storeName) {
-  inspectorTree.push({
-    id: storeName,
-    label: storeName
-  })
-  let groupId = (eventGroups += 1)
-  createLogger(app, api, store, storeName, groupId, storeName)
-}
-
-export function attachStores(app, stores) {
-  setupDevtoolsPlugin({ ...pluginConfig, app }, api => {
-    Object.entries(stores).forEach(([storeName, store]) => {
-      createStoreLogger(app, api, store, storeName)
-    })
-  })
+  return {
+    app,
+    createLogger,
+    eventGroups,
+    getGroupId,
+    inspectorTree,
+    isValidPayload,
+    layerId,
+    pluginConfig,
+    pushToInspectorTree
+  }
 }
