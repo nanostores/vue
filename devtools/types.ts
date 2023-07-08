2144e1b8717bab3778b84ca93535aa66105b3601
Eduard Aksamitov <e@euaaaio.ru>
2023-07-08 23:36:06 +0300

    chore: better types tests

---
+++ devtools/types.ts
@@ -0,0 +1,19 @@
+import { atom } from 'nanostores'
+import { createApp } from 'vue'
+
+import { devtools } from './index.js'
+
+let app = createApp({})
+let $atom = atom()
+
+devtools(
+  app,
+  { $atom },
+  {
+    getCreatorInspectorState: () => {},
+    ignoreActions: ['Increase Counter'],
+    messages: {
+      build: false
+    }
+  }
+)
